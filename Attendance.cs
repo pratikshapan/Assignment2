58322aa2088833e60eb8cc02f458a3231af1dcdc
pratikshapan <pratikshapandit94@gmail.com>
2022-08-19 13:06:01 +0530

    commit

---
+++ Attendance.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace Assignment2
+{
+    internal class Attendance
+    {
+		public static void Main12()
+		{
+			double a, b, p;
+			Console.WriteLine("Total number of Working Days ");
+			a = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Total number of days for absent");
+			b = Convert.ToInt32(Console.ReadLine());
+
+			p = a / b * 100;
+
+			if (p <= 75)
+			{
+				Console.WriteLine("Student will not able to Sit in Exam" + p);
+			}
+			else
+			{
+				Console.WriteLine("student is allowed to sit in exam");
+				Console.WriteLine();
+			}
+
+		}
+	}
+}
